518169dbf2ddf10788282984393be4552cd9fcc4
TarikYarasli <93830092+TarikYarasli@users.noreply.github.com>
2023-10-22 22:56:00 +0300

    Update Main.java
    for test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        
+        ShopAssistant[] myShopAssistantArray = FileIO.createShopAssistantArrayByFile("shopAssistants.csv");
+		System.out.println(myShopAssistantArray[99]); 
 
     }
 }
