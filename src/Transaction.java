1572d0dd76e1622b4016e199b350ce3191918814
Melis Sahin <130695976+Melis710@users.noreply.github.com>
2023-10-24 23:13:09 +0300

    Update Transaction.java

--- src/Transaction.java
+++ src/Transaction.java
@@ -6,57 +6,96 @@ public class Transaction {
 	
 	private int transactionID;
 	private Product[] products;
-	private int totalPrice;
-	private int transactionFee;
+	private double totalPrice;
+	private double transactionFee;
 	
 	private Random random = new Random();
 	private static final int LOWER_BOUND_QUANTITY = 1;
 	private static final int UPPER_BOUND_QUANTITY = 10;
-	private static int COUNTER = 0;
+	private static int COUNTER = 0; //to help assign transaction id
 	private int size = 0; 
 	private static final int NUMBER_OF_PRODUCTS = 3;
-	private FileIO fileIO = new FileIO();
 	
-	
-	public Transaction() {
+	public Transaction(Product[] products) {
+		if (products == null) {
+			throw new IllegalArgumentException("The product array cannot be null.");
+		}
 		this.transactionID = COUNTER++;
 		this.products = new Product[NUMBER_OF_PRODUCTS];
 		for (int index = 0; index < this.products.length; index++) {
-			int quantity = random.nextInt(LOWER_BOUND_QUANTITY, UPPER_BOUND_QUANTITY + 1);
-			addProduct(fileIO.getRandomProduct(), quantity);
+			addProduct(products);
 		}
 	}
 	
-	private void addProduct(Product product, int quantity) {
+	//copy constructor
+	public Transaction(Transaction source) {
+		this.transactionID = source.transactionID;
+		this.products = copyTransactionArray(source.products);
+		this.totalPrice = source.totalPrice;
+		this.transactionFee = source.transactionFee;
+	}
+	
+	/**addProductsFrom
+	 * adds a random product with a random quantity to the transaction from the array of products passed in  
+	 * @param products
+	 */
+	private void addProduct(Product[] products) {
+		
 		if (size == NUMBER_OF_PRODUCTS) {
-			System.out.printf("Transactions limited to %d products.", NUMBER_OF_PRODUCTS);
+			System.out.printf("Transaction limited to %d products.", NUMBER_OF_PRODUCTS);
 			return;
 		}
-		if (product == null || !(product instanceof Product)) {
-			throw new IllegalArgumentException("Product cannot be null and must be of type \"Product\"");
-		}
-		if (!(1 <= quantity && quantity <= 10)) {
-			throw new IllegalArgumentException("Quantity must be between 1 and 10.");
-		}
-		this.products[size++] = product;
-		this.totalPrice += product.getPrice()*quantity;
+		int randid = getRandomProductId(products.length);
+		//System.out.printf("\n************random product index: %d", randid);
+		Product randomProduct = new Product(products[randid]);
+		this.products[size++] = randomProduct;
+		this.totalPrice += randomProduct.getPrice() * getRandomQuantity(randomProduct);
+		
 	}
 	
-	public void setTransactionFee(int transactionFee) {
+	public void setTransactionFee(int transactionFeePercentage) {
 		if (transactionFee < 0) {
 			throw new IllegalArgumentException("Transaction fee cannot be less than 0.");
 		}
+		this.transactionFee = this.totalPrice * transactionFeePercentage / 100;
 	}
 	
 	@Override
 	public String toString() {
-		return "Transaction [transactionID=" + transactionID + ", products=" + Arrays.toString(products)
-				+ ", totalPrice=" + totalPrice + ", transactionFee=" + transactionFee  + ", size="
-				+ size + "]";
+		String info = "Transaction: " + transactionID + "\nProducts: \n";
+		for (int i = 0; i < this.products.length; i++) {
+			info += "\t" + this.products[i].toString() + "\n";
+		}
+		info += "Total Price: " + totalPrice + "\nTransaction Fee = " + transactionFee + "\n";
+		return info;
+	}
+	
+	private Product[] copyTransactionArray(Product[] products) {
+		Product[] newArr = new Product[products.length];
+		for (int index = 0; index < products.length; index++) {
+			newArr[index] = new Product(products[index]);
+		}return newArr;
+	}
+	
+	private int getRandomProductId(int bound) {
+		int randomId = random.nextInt(bound);
+		return randomId;
+	}
+	
+	private int getRandomQuantity(Product product) {
+		int randomQuantity = random.nextInt(LOWER_BOUND_QUANTITY, UPPER_BOUND_QUANTITY+1);
+		product.setQuantity(randomQuantity);
+		return randomQuantity;
 	}
 
+	public double getTotalPrice() {
+		return this.totalPrice;
+	}
+	
 	public static void main(String[] args) {
-		Transaction transaction = new Transaction();
+		Product[] products = {new Product(3, "banana", 38, 10), new Product(4, "strawberry", 45, 5), new Product(5, "kiwi", 22, 3)};
+		Transaction transaction = new Transaction(products);
+		System.out.println(transaction.getTotalPrice());
 		System.out.println(transaction);
 	}
 
