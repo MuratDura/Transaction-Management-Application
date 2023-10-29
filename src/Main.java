9b791971b9e256ff4da7241a3df7172da0fd5fe7
TarikYarasli <93830092+TarikYarasli@users.noreply.github.com>
2023-10-29 16:47:32 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        ShopAssistant[] myShopAssistantArray = FileIO.createShopAssistantArrayByFile("shopAssistants.csv");
-	System.out.println(myShopAssistantArray[99]); 
+package transactionManagement;
+import helper.FileIO;
 
-    }
+public class Main {
+
+	public static void main(String[] args) {
+		// For test
+		ShopAssistant[] myShopAssistantArray = FileIO.createShopAssistantArrayByFile("shopAssistants.csv");
+		System.out.println(myShopAssistantArray[99]);
+		Product[] myProductArray = FileIO.createProductArrayByFile("products.csv");
+		System.out.println(myProductArray[89]);
+		Transaction transaction1 = new Transaction(myProductArray);
+		System.out.println(transaction1);
+	}
+
 }
